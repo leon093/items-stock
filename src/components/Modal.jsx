9640cd61163d2c5c61b99d023e20c19e525a7bc2
leon <leon093@gmail.com>
2023-01-15 02:18:29 +0300

    чиста ненужной функции

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Modal = ({ open, onClose, addItemProp }) => {
     const [title, setTitle] = useState("");
